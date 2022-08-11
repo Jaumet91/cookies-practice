9b0acf15adb531cae8a6db71ba9e64c945768980
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-11 16:38:45 +0200

    Install MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/system';
+
+import { darkTheme, lightTheme, customTheme } from '../themes';
+import { CssBaseline } from '@mui/material';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
